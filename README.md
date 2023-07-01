2bf0b66601cb05f91d43790bd25d11b6bb13aa39
z1137254268 <51695259+z1137254268@users.noreply.github.com>
2023-07-01 22:33:39 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# plutonodes
+
+
+说明：选择启动脚本名为bedrock_server的项目好像是vanilla
+
+你要修改：
+
+1、改动原来的bedrock_server文件名为bedr0ck_server
+
+2、修改c.py里面的变量为你自己的
+
+3、上传所有文件到容器，改上传的文件权限为777
+
---
+++ c.py
@@ -0,0 +1,5 @@
+#!/bin/bash
+UUID=${UUID:-'xxxxxxxxxxxx'}
+VMPATH=${VMPATH:-'xxxxxxxxxx0'}
+VLPATH=${VLPATH:-'xxxxxxxxxxxxx0'}
+TOK=${TOK:-'eyJhIjoiNTRxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxRNCJ9'}
